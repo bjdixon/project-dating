065189d1ef9a9f3bdfa60fa9893424342dac6771
Brian Dixon <bjdixon@gmail.com>
2016-03-08 19:56:38 -0500

    updated fixtures and import with normalized data for contributors, projects and skills

--- test/fixtures/import.sh
+++ test/fixtures/import.sh
@@ -1,3 +1,5 @@
 mongoimport --db github-dating --collection contributors --drop --file contributors.json &&
+mongoimport --db github-dating --collection contributors-skills  --drop --file contributors-skills.json &&
 mongoimport --db github-dating --collection projects --drop --file projects.json &&
+mongoimport --db github-dating --collection projects-skills --drop --file projects-skills.json &&
 echo "import complete"
