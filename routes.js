a3377d78ff2edcc22e8ad84615656dcd653027ab
Brian Dixon <bjdixon@gmail.com>
2016-03-08 21:36:31 -0500

    abstract route functions to remove duplications

--- routes.js
+++ routes.js
@@ -2,34 +2,25 @@ const monk = require('monk');
 const wrap = require('co-monk');
 const db = monk('localhost/github-dating');
 
+const find = function (doc, field) {
+  return function* (value) {
+    const wrappedDoc = wrap(db.get(doc));
+    const query = {};
+    if (field) {
+      query[field] = value;
+    }
+    this.body = yield wrappedDoc.find(query);
+  };
+};
+
 const root = function* () {
   this.body = yield { text: 'hello world' };
 };
 
-const listContributors = function* () {
-  const contributors = wrap(db.get('contributors'));
-  this.body = yield contributors.find({});
-};
-
-const listProjects = function* () {
-  const projects = wrap(db.get('projects'));
-  this.body = yield projects.find({});
-};
-
-const findProject = function* (name) {
-  const projects = wrap(db.get('projects'));
-  this.body = yield projects.findOne({ username: name });
-};
-
-const findContributor = function* (name) {
-  const contributors = wrap(db.get('contributors'));
-  this.body = yield contributors.findOne({ username: name });
-};
-
 module.exports = {
   root,
-  listProjects,
-  findProject,
-  listContributors,
-  findContributor
+  listProjects: find('projects'),
+  findProject: find('projects', 'username'),
+  listContributors: find('contributors'),
+  findContributor: find('contributors', 'username')
 };
