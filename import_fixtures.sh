cbd59e084dbaef4f435e543dc58d688377dd2bd4
Brian Dixon <bjdixon@gmail.com>
2016-03-09 19:56:45 -0500

    move fixtures import script, add test to package.json, add ramda :sheep:

--- import_fixtures.sh
+++ import_fixtures.sh
@@ -1,5 +1,7 @@
+cd ./test/fixtures &&
 mongoimport --db github-dating --collection contributors --drop --file contributors.json &&
 mongoimport --db github-dating --collection contributors-skills  --drop --file contributors-skills.json &&
 mongoimport --db github-dating --collection projects --drop --file projects.json &&
 mongoimport --db github-dating --collection projects-skills --drop --file projects-skills.json &&
+cd ./../../ &&
 echo "import complete"
